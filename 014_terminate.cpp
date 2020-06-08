d3b01758aa0418b4a66ef21be759913dbf0ca8b7
Yuri Dyachenko <pub.yurial@gmail.com>
2020-06-08 09:00:07 +0300

    new file:   014_terminate.cpp

---
+++ 014_terminate.cpp
@@ -0,0 +1,33 @@
+#include <yurco/all.hpp>
+#include <iostream>
+#include <stdlib.h>
+
+void entry(yurco::Coroutine& self)
+    {
+    try
+        {
+        for (;;)
+            self.yield();
+        }
+    catch (const yurco::terminate_exception&)
+        {
+        std::cout << "Coroutine should be terminated" << std::endl;
+        }
+    }
+
+int main()
+    {
+    yurco::Coroutine coro(yurco::Stack(16*1024), entry);
+    coro();
+    try
+        {
+        coro.set_exception(std::make_exception_ptr(yurco::terminate_exception()));
+        coro();
+        std::cout << "No exception" << std::endl;
+        }
+    catch (const yurco::terminate_exception&)
+        {
+        std::cout << "Terminate exception was returned" << std::endl;
+        }
+    return EXIT_SUCCESS;
+    }
